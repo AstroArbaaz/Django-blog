f5617729ea48adcd90b9a3e286fbbb31daf21fd9
Arbaaz khan <mrkhan4231@gmail.com>
2020-04-28 16:06:37 +0530

    test changes in Users/views.py corrected template urls typo

--- Users/views.py
+++ Users/views.py
@@ -15,7 +15,7 @@ def register(request):
             return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'Users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
@@ -37,4 +37,4 @@ def profile(request):
         'p_form': p_form
     }
 
-    return render(request, 'Users/profile.html', context)
+    return render(request, 'users/profile.html', context)
